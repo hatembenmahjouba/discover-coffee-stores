eb6803bc78a63e668d101d02d69fc700e131ebf7
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-09-19 16:25:13 +0200

    adding near me to home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import Banner from '../components/banner';
 import Card from '../components/card';
+import useTrackLocation from '../hooks/use-track-location';
 import { fetchCoffeeStores } from '../lib/coffee-stores';
 
 import styles from '../styles/Home.module.css';
@@ -14,10 +16,29 @@ export async function getStaticProps(context) {
     },
   };
 }
-
 export default function Home(props) {
+  const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
+    useTrackLocation();
+
+  const [coffeeStores, setCoffeeStores] = useState('');
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null);
+  useEffect(() => {
+    if (latLong) {
+      async function fetchMyAPI() {
+        try {
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
+          setCoffeeStores(fetchedCoffeeStores);
+        } catch (err) {
+          console.log(err);
+          setCoffeeStoresError(err);
+        }
+      }
+      fetchMyAPI();
+    }
+  }, [latLong]);
+
   const handleOnBannerBtnClick = () => {
-    console.log('hi');
+    handleTrackLocation();
   };
   return (
     <div className={styles.container}>
@@ -29,9 +50,11 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <Banner
-          buttonText='View stores nearby'
+          buttonText={isFindingLocation ? 'Locating...' : 'View stores nearby'}
           handleOnClick={handleOnBannerBtnClick}
         />
+        {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
+        {coffeeStoresError && <p>Something went wrong: {coffeeStoresError}</p>}
         <div className={styles.heroImage}>
           <Image
             src='/static/hero-image.png'
@@ -40,8 +63,31 @@ export default function Home(props) {
             alt='hero'
           />
         </div>
+
+        {coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores near me</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    className={styles.card}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
+
         {props.coffeeStores.length > 0 && (
-          <>
+          <div className={styles.sectionWrapper}>
             <h2 className={styles.heading2}>Toronto stores</h2>
             <div className={styles.cardLayout}>
               {props.coffeeStores.map((coffeeStore) => {
@@ -59,7 +105,7 @@ export default function Home(props) {
                 );
               })}
             </div>
-          </>
+          </div>
         )}
       </main>
     </div>
