4095b908092f04d31983217bdd3a0b42c8bc590f
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-09-22 00:28:37 +0200

    create coffee store API

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,47 @@
+import { table, getMinifiedRecords } from '../../lib/airtable';
+
+const createCoffeeStore = async (req, res) => {
+  if (req.method === 'POST') {
+    const { id, name, neighbourhood, address, voting, imgUrl } = req.body;
+    try {
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id=${id}`,
+          })
+          .firstPage();
+
+        if (findCoffeeStoreRecords.length !== 0) {
+          const records = getMinifiedRecords(findCoffeeStoreRecords);
+          res.json(records);
+        } else {
+          if (name) {
+            const createRecords = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                },
+              },
+            ]);
+            const records = getMinifiedRecords(createRecords);
+            res.status(201).json({ records });
+          } else {
+            res.status(400).json({ message: 'Id or name is missing' });
+          }
+        }
+      } else {
+        res.status(400).json({ message: 'Id is missing' });
+      }
+    } catch (err) {
+      console.error('Error creating or finding a store', err);
+      res.status(500).json({ message: 'Error creating or finding a store' });
+    }
+  }
+};
+
+export default createCoffeeStore;
